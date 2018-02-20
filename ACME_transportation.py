c9691b724ef30b0783c77a2bdb10be8993523377
paeltech <pmandele9@gmail.com>
2018-02-20 17:42:47 +0300

    Addes power comparison function

--- ACME_transportation.py
+++ ACME_transportation.py
@@ -13,15 +13,28 @@ def csv_dict_reader(file_obj):
 
 def data_check(reader):
     dt = 0
-    dr = 0
+    dr = 1011.7959
     for line in reader:
         dt += float(line["distance_travelled"])
         # dr -= float(line["distance_remaining"])
-    print dt - 1011.7959
-    # print dr
+    print dt - dr
+
+    #Write to file if there is a problem
+    #if dt - dr == dr:
+    #    print ("there is no problem in sensor readings")
+
+def power_check(reader):
+    for line in reader:
+        if line["power_supply"] > 0.995 or line["power_supply == 1"]:
+            #write the error to file
+            print ("There is power fluctuation at {}", format(line))
 
 
 
 if __name__ == "__main__":
     with open("at_distance_sensors.csv") as f_obj:
         csv_dict_reader(f_obj)
+
+if __name__ == "__main__":
+    with open("at_power_sensors.csv") as f_obj1:
+        csv_dict_reader(f_obj1)
